f76258a477cc2055c3e92a1081f11cc3f5a54644
wangchengbo <hi_boyo@163.com>
2016-10-28 14:22:50 +0800

    Initial commit

---
+++ app/src/main/java/com/boyo/camerafilterfbo/widget/CameraGLSurfaceView.java
@@ -0,0 +1,20 @@
+package com.boyo.camerafilterfbo.widget;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+/**
+ * Created by wangchengbo on 2016/10/28.
+ */
+
+public class CameraGLSurfaceView extends GLSurfaceView{
+
+    public CameraGLSurfaceView(Context context) {
+        super(context);
+    }
+
+    public CameraGLSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/boyo/camerafilterfbo/widget/CameraTextureView.java
@@ -0,0 +1,8 @@
+package com.boyo.camerafilterfbo.widget;
+
+/**
+ * Created by wangchengbo on 2016/10/28.
+ */
+
+public class CameraTextureView {
+}
